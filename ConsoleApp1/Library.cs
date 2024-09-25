aa69f5c5c7bed06d4fd7e31aeec7b214f98e3c38
carlos.avilez <alejandroavilez56@hotmail.com>
2024-09-25 13:31:47 -0700

    Add project files.

---
+++ ConsoleApp1/Library.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Library
+    {
+        public void AddBook(Book book) { }
+
+    public void RemoveBook(string isbn) { }
+
+        public void CheckOutBook(string isbn) { }
+        public void ReturnBook(string isbn) { }
+
+        public List<Book> ListAvailableBooks()
+        {
+            return new List<Book>();
+        }
+
+    }
+}
